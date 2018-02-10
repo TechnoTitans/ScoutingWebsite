d0cf55f4817e86290d72dc72cc1c8946904188ae
Y. S <ThePhantomGamer@users.noreply.github.com>
2018-02-10 09:05:44 -0500

    Add basis for app
    Signed-off-by: Y. S <ThePhantomGamer@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# ScoutingWebsite
+# Scouting App
+
+### About
+This app will be used for scouting in the 2018 frc season
---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var app = new Vue({
+    template: './views/main.html',
+    data() {
+        return {
+            kabl: 'aasf'
+        }
+    }
+});
