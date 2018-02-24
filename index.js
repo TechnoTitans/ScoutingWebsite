44646a89371ea86763d83e3e1de59c851d2ff3af
Yaseen <9275716+ynx0@users.noreply.github.com>
2018-02-24 10:42:27 -0800

    Delete index.js as it is not needed
    Signed-off-by: Yaseen Shaikh <yaseentheepic@gmail.com>

--- index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-import express from "express";
-const app = express();
-
-const port = 80;
-
-// todo make project structure conform to firebase static/dynamic dirs etc
-// app.use(express.static(staticFolder));
-app.listen(port);
