96e3958ebfc21d172e73a275e3be470624c631e3
Praneeth Kolichala <praneeth.kolichala@gmail.com>
2018-03-07 22:26:59 -0500

    Removed useless authentication options

--- public/main.js
+++ public/main.js
@@ -247,11 +247,7 @@ document.addEventListener('init', function (event) {
           signInOptions: [
             // Leave the lines as is for the providers you want to offer your users.
             firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-            firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-            firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-            firebase.auth.GithubAuthProvider.PROVIDER_ID,
             firebase.auth.EmailAuthProvider.PROVIDER_ID,
-            firebase.auth.PhoneAuthProvider.PROVIDER_ID
           ],
         };
         ui.start('#firebaseui-auth-container', uiConfig);
