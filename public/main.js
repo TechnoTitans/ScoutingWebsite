3769b57fa78fa09f9c7d524fe5cc8189e41df425
Praneeth Kolichala <praneeth.kolichala@gmail.com>
2018-03-08 22:04:25 -0500

    No need to re-login every time

--- public/main.js
+++ public/main.js
@@ -242,22 +242,19 @@ document.addEventListener('init', function (event) {
         let ui = new firebaseui.auth.AuthUI(firebase.auth());
         let uiConfig = {
           callbacks: {
-            signInSuccess: function(currentUser, credential, redirectUrl) {
-              // User successfully signed in.
-              // Return type determines whether we continue the redirect automatically
-              // or whether we leave that to developer to handle.
-              if (currentUser) document.getElementById('appNavigator').pushPage('tabbar.html');
-              return false;
-            }
+              signInSuccess: function(currentUser, credential, redirectUrl) {
+                  return false;
+              }
           },
-          // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
           signInOptions: [
-            // Leave the lines as is for the providers you want to offer your users.
             firebase.auth.GoogleAuthProvider.PROVIDER_ID,
             firebase.auth.EmailAuthProvider.PROVIDER_ID,
           ],
         };
         ui.start('#firebaseui-auth-container', uiConfig);
+        firebase.auth().onAuthStateChanged(function(user) {
+            if (user) document.getElementById('appNavigator').pushPage('tabbar.html');
+        });
     }
 
     // this.querySelector('ons-toolbar div.center').textContent = this.data.title;
